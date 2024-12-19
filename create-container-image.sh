04588be21c995cf0710a6c57bc028d650c8b5c6e
Josef Hoerandtner <josef.hoerandtner@springer.com>
2024-12-19 12:19:15 +0000

    Add helper scripts

---
+++ create-container-image.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./gradlew clean assemble
+pack build spring-music --path ./build/libs --builder paketobuildpacks/builder-jammy-base
+docker tag spring-music eu.gcr.io/halfpipe-io/spring-music:latest
+docker push eu.gcr.io/halfpipe-io/spring-music:latest
