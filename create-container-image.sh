71a57660fe98a6fbca852b385737c799facadb58
Rob Whitby <rob.whitby@springer.com>
2025-06-19 15:05:34 +0100

    add cloudnative buildpack example

--- create-container-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pack build spring-music \
-  --path ./build/libs \
-  --builder paketobuildpacks/builder-jammy-base \
-  --buildpack paketobuildpacks/builder-jammy-base
-docker tag spring-music eu.gcr.io/halfpipe-io/spring-music:latest
-docker push eu.gcr.io/halfpipe-io/spring-music:latest
